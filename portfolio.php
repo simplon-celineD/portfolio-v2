9595c9cfb67a514a5e0637156fa7bf30fc5cd6c0
simplon-celineD <celinedesbois.cd@gmail.com>
2016-04-25 16:28:27 +0200

    4eme sauvegarde

--- portfolio.php
+++ portfolio.php
@@ -201,9 +201,19 @@
               </div>
             </article>
 
-            <?php
-            include("contact.php");
-            ?>
+            <article id="formulaire">
+              <form action="result.php" method="POST">
+                  <input type="text" name="name" id="name" placeholder="Votre Nom ..."/>
+                  <br/>
+                  <input type="text" name="forname" id="forname" placeholder="Votre Prenom ..."/>
+                  <br/>
+                  <input type="email" name="email" id="email" placeholder="Votre adresse mail ..."/>
+                  <br/>
+                  <textarea type="text" name="textarea" id="textarea"> Votre Message ... </textarea>
+                  <br/>
+                  <input type="submit" id="bouton">  </input>
+              </form>
+            </article>
 
           </article>
 
--- result.php
+++ result.php
@@ -8,7 +8,9 @@
 
   <body>
 
-    <p> Bonjour <?php echo htmlspecialchars($_POST['name']).' '.htmlspecialchars($_POST['forname']); ?> </br> votre email : <?php echo htmlspecialchars($_POST['email']); ?> </br> Votre message : <?php echo htmlspecialchars($_POST['textarea']); ?> </p>
+    <p> Bonjour <?php echo htmlspecialchars($_POST['name']) . ' ' . htmlspecialchars($_POST['forname']); ?> </p>
+    <p> votre email : <?php echo htmlspecialchars($_POST['email']); ?> </p>
+    <p> Votre message : <?php echo htmlspecialchars($_POST['textarea']); ?> </p>
 
 
   </body>
